64063a7b864d364792e61bf9e41254e847e3f48c
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 15:37:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,15 @@
 def prime?(int)
   
   check_numbers = [2,3,5,7]
-  if( int > 2 && int.even?)
+  if( int == 0 || int == 1)
+    return false
+  elsif( int > 2 && int.even?)
     return false
   elsif check_numbers.include?(int)
-  return true
+    return true
+  elsif int > 7 && int.odd?
+    check_numbers.any? do |check| int/check
+  end
   
   
 end
